d246fa7180b67ed179186c6e46940382d038c51b
noni <dluna1011@gmail.com>
2016-10-24 11:37:01 +0900

    Layout basic

---
+++ app/src/main/java/com/giocoso/administrator/shoom/selftestfragment/selftestchecklist.java
@@ -0,0 +1,30 @@
+package com.giocoso.administrator.shoom.selftestfragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.giocoso.administrator.shoom.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class selftestchecklist extends Fragment {
+
+
+    public selftestchecklist() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_selftestchecklist, container, false);
+    }
+
+}
